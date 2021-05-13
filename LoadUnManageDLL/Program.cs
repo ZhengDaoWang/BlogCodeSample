24750ff17791f1aa4d939c32e49341b88ed8bd58
王政道 <wangzhengdao@cvte.com>
2021-05-13 11:15:42 +0800

    add UnLoadManageDLL project

---
+++ LoadUnManageDLL/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.ComponentModel;
+using System.IO;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+namespace LoadUnManageDLL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UnzipAndLoad();
+        }
+
+        /// <summary>
+        /// 解压资源并且加载非托管DLL
+        /// </summary>
+        static void UnzipAndLoad()
+        {
+            var folderPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            var dllPath = Path.Combine(folderPath, $"{nameof(Resource.pdfium)}.dll");
+            File.WriteAllBytes(dllPath,Resource.pdfium);
+            LoadDll(dllPath);
+        }
+
+        ///// <summary>.
+        ///// managed wrapper around LoadLibrary
+        ///// </summary>
+        ///// <param name="dllName"></param>
+        public static void LoadDll(string dllName)
+        {
+            IntPtr h = LoadLibrary(dllName);
+            if (h == IntPtr.Zero)
+            {
+                Exception e = new Win32Exception();
+                throw new DllNotFoundException($"Unable to load library: {dllName}", e);
+            }
+
+            Console.WriteLine("Load library successful");
+        }
+
+        [DllImport("kernel32", SetLastError = true, CharSet = CharSet.Unicode)]
+        static extern IntPtr LoadLibrary(string lpFileName);
+    }
+}
