034344574e38d253d1fcb59a77f09f31acbf8f96
wangzhengdao <wangzhengdao@cvte.com>
2024-03-01 17:21:40 +0800

    新增新的链接方式

--- CreateOpenXmlHyperlinkSample/CreateOpenXmlHyperlinkSample/Program.cs
+++ CreateOpenXmlHyperlinkSample/CreateOpenXmlHyperlinkSample/Program.cs
@@ -1,6 +1,8 @@
-﻿using DocumentFormat.OpenXml.Drawing;
+﻿using System.Reflection;
+using DocumentFormat.OpenXml.Drawing;
 using DocumentFormat.OpenXml.Packaging;
 using DocumentFormat.OpenXml.Presentation;
+using Path = System.IO.Path;
 using Shape = DocumentFormat.OpenXml.Presentation.Shape;
 
 namespace CreateOpenXmlHyperlinkSample
@@ -9,8 +11,14 @@ namespace CreateOpenXmlHyperlinkSample
     {
         static void Main(string[] args)
         {
+
+            var mainExecuteDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+
+            var pptFilePath = Path.Combine(mainExecuteDirectory!, "YourPresentation.pptx");
+
+
             // 打开一个PPTX文档
-            using PresentationDocument presentationDocument = PresentationDocument.Open(@"C:\Users\11019\Downloads\YourPresentation.pptx", true);
+            using PresentationDocument presentationDocument = PresentationDocument.Open(pptFilePath, true);
 
             var slideIdList = presentationDocument.PresentationPart!.Presentation.SlideIdList;
             if (slideIdList is null || !slideIdList.Any())
@@ -29,13 +37,38 @@ namespace CreateOpenXmlHyperlinkSample
             var targetSlidePart = (SlidePart) presentationDocument.PresentationPart.GetPartById(targetSlideId.RelationshipId!.Value!);
             var relationshipId = firstSlidePart.CreateRelationshipToPart(targetSlidePart);
 
-            var shape = firstSlidePart.Slide.CommonSlideData!.ShapeTree!.GetFirstChild<Shape>();
-            shape!.NonVisualShapeProperties!.NonVisualDrawingProperties!.HyperlinkOnClick = new HyperlinkOnClick()
+            var shapeElements = firstSlidePart.Slide.CommonSlideData!.ShapeTree!.Elements<Shape>().ToList();
+
+            //第一个形状设置跳转第三页
+            shapeElements[0]!.NonVisualShapeProperties!.NonVisualDrawingProperties!.HyperlinkOnClick = new HyperlinkOnClick()
             {
                 Action = PptAction.SlideJump,
                 Id = relationshipId
             };
 
+
+            //第二个形状设置跳转下一页
+            var shapeElement = shapeElements[1];
+            shapeElement.NonVisualShapeProperties!.NonVisualDrawingProperties!.HyperlinkOnClick = new HyperlinkOnClick()
+            {
+                Action = PptAction.JumpNextSlide,
+            };
+
+            var filePath = Path.Combine(mainExecuteDirectory!, "两只老虎-原声.mp3");
+            var fileHyperlinkRelationship = firstSlidePart.AddHyperlinkRelationship(new Uri(filePath, UriKind.Absolute), true);
+            //第三个形状设置打开文件
+            shapeElements[2]!.NonVisualShapeProperties!.NonVisualDrawingProperties!.HyperlinkOnClick = new HyperlinkOnClick()
+            {
+                Action = PptAction.OpenFile,
+                Id = fileHyperlinkRelationship.Id
+            };
+
+            //第四个形状设置打开网页链接
+            var httpHyperlinkRelationship = firstSlidePart.AddHyperlinkRelationship(new Uri($"http://www.baidu.com", UriKind.Absolute), true);
+            shapeElements[3]!.NonVisualShapeProperties!.NonVisualDrawingProperties!.HyperlinkOnClick = new HyperlinkOnClick()
+            {
+                Id = httpHyperlinkRelationship.Id
+            };
 
             // 保存并关闭文档
             presentationDocument.Save();
@@ -48,5 +81,16 @@ namespace CreateOpenXmlHyperlinkSample
         /// 跳转页面
         /// </summary>
         public const string SlideJump = "ppaction://hlinksldjump";
+
+        /// <summary>
+        /// 跳转下一页
+        /// </summary>
+        public const string JumpNextSlide = "ppaction://hlinkshowjump?jump=nextslide";
+
+        /// <summary>
+        /// 打开文件
+        /// </summary>
+        public const string OpenFile = "ppaction://hlinkfile";
+
     }
 }
